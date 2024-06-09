4ca0e49d18d278613bd21571ba16a4bd622a4f54
jonatansilva <jonatanemanuelsilva6@gmail.com>
2024-06-08 21:45:03 -0300

    first commit

---
+++ API/Controllers/ProductoController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Microsoft.AspNetCore.Mvc;
+using Models.Dtos;
+using Models.Entidades;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<ProductoDto>> GetProductos()
+        {
+            return ProductoData.listaProducto
+                   .Select(p => new ProductoDto
+                   {
+                       NombreProducto = p.NombreProducto,
+                       Categoria = p.Categoria,
+                       Marca = p.Marca,
+                       Precio = p.Precio,
+                       Costo = p.Costo
+                   }).ToList();
+        }
+    }
+}
---
+++ Models/Entidades/Categoria.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Entidades
+{
+    [Table("Categoria")]
+    public class Categoria
+    {
+        [Key]
+        [Column("CategoriaId")]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
